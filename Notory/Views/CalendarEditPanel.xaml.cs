b36a512296b5de9ba2834efb8f8cccc32b8f3274
Luis Allamand <Luis@g-12.ch>
2025-03-15 01:09:07 +0100

    Updated the Design of the Calendar

--- Notory/Views/CalendarEditPanel.xaml.cs
+++ Notory/Views/CalendarEditPanel.xaml.cs
@@ -1,16 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Notory.Views
 {
@@ -20,69 +11,129 @@ namespace Notory.Views
 
         // Selected Date for the DayScheduleView
         public DateTime SelectedDate { get; private set; }
+
         public CalendarEditPanel()
         {
             InitializeComponent();
             _currentDate = DateTime.Now;
-            SelectedDate = _currentDate;
+            SelectedDate = _currentDate; // Setzt das heutige Datum als ausgewähltes Datum
             UpdateCalendar();
         }
 
         private void UpdateCalendar()
         {
-          CalendarGrid.Children.Clear();
+            CalendarGrid.Children.Clear();
+            CalendarGrid.RowDefinitions.Clear();
+            CalendarGrid.ColumnDefinitions.Clear();
 
-          MonthYearText.Text = _currentDate.ToString("MMMM yyyy");
+            MonthYearText.Text = _currentDate.ToString("MMMM yyyy");
 
-          DateTime firstDayOfMonth = new DateTime(_currentDate.Year, _currentDate.Month, 1);
-
-          int startDay = (int)firstDayOfMonth.DayOfWeek;
-          if (startDay == 0) startDay = 7; 
-
-          int daysInMonth = DateTime.DaysInMonth(_currentDate.Year, _currentDate.Month);
-
-          int totalCells = 6 * 7;
-          int currentDay = 1;
-
-          for (int i = 1; i <= totalCells; i++)
-          {
-            Button dayButton = new Button
+            // Zeilen für Wochentage und Datumsbuttons hinzufügen
+            CalendarGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto }); // Zeile für Wochentage
+            for (int i = 0; i < 6; i++) // 6 Zeilen für die Datumsbuttons
             {
-              Margin = new Thickness(2),
-              FontSize = 16,
-              Background = Brushes.White,
-              Foreground = Brushes.Black
-            };
-
-            if (i < startDay || currentDay > daysInMonth)
-            {
-              DateTime displayDate;
-              if (i < startDay)
-              {
-                displayDate = firstDayOfMonth.AddDays(i - startDay);
-              }
-              else
-              {
-                displayDate = firstDayOfMonth.AddDays((currentDay - 1) + (i - startDay - daysInMonth + 1));
-              }
-
-              dayButton.Content = displayDate.Day.ToString();
-              dayButton.Background = Brushes.LightGray; 
-              dayButton.Foreground = Brushes.Gray;
-              dayButton.IsEnabled = false; 
+                CalendarGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
             }
-            else
+
+            // Spalten für die 7 Wochentage hinzufügen
+            for (int i = 0; i < 7; i++)
             {
-              dayButton.Content = currentDay.ToString();
-              if (currentDay == DateTime.Now.Day && _currentDate.Month == DateTime.Now.Month && _currentDate.Year == DateTime.Now.Year)
-              {
-                dayButton.Background = Brushes.LightBlue; 
-              }
-              currentDay++;
+                CalendarGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
             }
-            dayButton.Click += DayButton_Click;
-            CalendarGrid.Children.Add(dayButton);
-          }
+
+            // Wochentagsnamen hinzufügen
+            string[] weekDays = { "MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN" };
+            for (int i = 0; i < 7; i++)
+            {
+                TextBlock dayName = new TextBlock
+                {
+                    Text = weekDays[i],
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    FontSize = 16,
+                    Foreground = Brushes.White
+                };
+                Grid.SetRow(dayName, 0); // Erste Zeile
+                Grid.SetColumn(dayName, i); // Spalte i
+                CalendarGrid.Children.Add(dayName);
+            }
+
+            // Datumsbuttons hinzufügen
+            DateTime firstDayOfMonth = new DateTime(_currentDate.Year, _currentDate.Month, 1);
+            int startDay = (int)firstDayOfMonth.DayOfWeek;
+            if (startDay == 0) startDay = 7; // Sonntag auf 7 setzen
+
+            int daysInMonth = DateTime.DaysInMonth(_currentDate.Year, _currentDate.Month);
+            int totalCells = 6 * 7;
+            int currentDay = 1;
+
+            for (int i = 1; i <= totalCells; i++)
+            {
+                Button dayButton = new Button
+                {
+                    Margin = new Thickness(2),
+                    FontSize = 16,
+                    Background = Brushes.Transparent,
+                    Foreground = Brushes.White,
+                    BorderBrush = Brushes.Transparent,
+                    Padding = new Thickness(0),
+                    HorizontalContentAlignment = HorizontalAlignment.Center,
+                    VerticalContentAlignment = VerticalAlignment.Center,
+                    Template = (ControlTemplate)this.Resources["RoundButtonTemplate"]
+                };
+
+                if (i < startDay || currentDay > daysInMonth)
+                {
+                    DateTime displayDate;
+                    if (i < startDay)
+                    {
+                        displayDate = firstDayOfMonth.AddDays(i - startDay);
+                    }
+                    else
+                    {
+                        displayDate = firstDayOfMonth.AddDays((currentDay - 1) + (i - startDay - daysInMonth + 1));
+                    }
+
+                    //dayButton.Content = displayDate.Day.ToString();
+                    //dayButton.Background = Brushes.DarkGray;
+                    //dayButton.Foreground = Brushes.White;
+                    //dayButton.Opacity = 0.2;
+                    //dayButton.IsEnabled = false;
+                }
+                else
+                {
+                    dayButton.Content = currentDay.ToString();
+                    DateTime buttonDate = new DateTime(_currentDate.Year, _currentDate.Month, currentDay);
+
+                    if (buttonDate == SelectedDate)
+                    {
+                        dayButton.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#5c4ef8"));
+                    }
+                    else if (buttonDate == DateTime.Today)
+                    {
+                        if(_currentDate != SelectedDate)
+                        {
+                            dayButton.Background = Brushes.Transparent;
+                        }
+                        else
+                        {
+                            dayButton.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#5c4ef8"));
+                        }
+                        dayButton.Foreground = Brushes.White;
+                    }
+                    currentDay++;
+                }
+
+                dayButton.Click += DayButton_Click;
+
+                // Position des Buttons im Grid festlegen
+                int row = (i + startDay - 2) / 7 + 1; // Zeile berechnen
+                int col = (i + startDay - 2) % 7; // Spalte berechnen
+
+                Grid.SetRow(dayButton, row);
+                Grid.SetColumn(dayButton, col);
+                CalendarGrid.Children.Add(dayButton);
+            }
         }
 
         private void DayButton_Click(object sender, RoutedEventArgs e)
@@ -91,20 +142,21 @@ namespace Notory.Views
             int selectedDay = int.Parse(button.Content.ToString());
 
             SelectedDate = new DateTime(_currentDate.Year, _currentDate.Month, selectedDay);
+            UpdateCalendar();
+
             MessageBox.Show($"Selected Date: {_currentDate.ToString("MMMM")} {button.Content}, {_currentDate.Year}");
-            
         }
 
         private void PrevMonthButton_Click(object sender, RoutedEventArgs e)
         {
-          _currentDate = _currentDate.AddMonths(-1);
-          UpdateCalendar();
+            _currentDate = _currentDate.AddMonths(-1);
+            UpdateCalendar();
         }
 
         private void NextMonthButton_Click(object sender, RoutedEventArgs e)
         {
-          _currentDate = _currentDate.AddMonths(1);
-          UpdateCalendar();
+            _currentDate = _currentDate.AddMonths(1);
+            UpdateCalendar();
         }
-  }
+    }
 }
