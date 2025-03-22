2e2b488ecaefc5448d53eb7c38b5be11eb232a2a
Luis Allamand <Luis@g-12.ch>
2025-03-22 22:36:28 +0100

    Added a Test Class to make a Mongodb entry

---
+++ Notory/Helpers/MongoDB.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Notory.Helpers
+{
+    internal class MongoDB
+    {
+        public void SetPost()
+        {
+            string connectionString = "mongodb://127.0.0.1:27017";
+            var client = new MongoClient(connectionString);
+
+            var database = client.GetDatabase("Notory");
+
+            var collection = database.GetCollection<BsonDocument>("CalendarPost");
+
+            var document = new BsonDocument
+            {
+                {"Title", "Exam.net downloaded" },
+                {"Date", "19.06.2020" },
+                {"Text", "Blablabla bliblibliblibli, hubububububububububububububuububub, linganguliguligu niwapa linganggu linganggu" }
+            };
+
+            collection.InsertOne(document);
+            Console.WriteLine("Dokument eingeführt");
+        }
+    }
+}
